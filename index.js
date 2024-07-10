e301a037742574c857721ccd73ad2a8613cdd623
KIM SEON WOO <sunwoo005@naver.com>
2024-07-11 03:28:40 +0900

    [#1] Chore : 개발환경 세팅
    Link.Zip 프로젝트 개발환경 세팅
    
    Reviewd-by: meoraeng\nRefs: #1

---
+++ index.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+import express from 'express'
+import cors from 'cors';
+import SwaggerUi from 'swagger-ui-express';
+import { specs } from './config/swagger.config.js';
+import { status } from './config/response.status.js';
+import { response } from './config/response.js';
+
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
+app.use(cors());                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록
+app.use(express.urlencoded({extended: true})); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+
+
+app.get('/', function (req, res) {
+    res.send('express 테스트')
+})
+
+app.use((err, req, res, next) => {
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;   
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`✅ 서버를 ${app.get('port')}번 포트에서 열었습니다.`);
+});
