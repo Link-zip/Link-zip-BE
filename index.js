2dae6f454191633cc6e91a11209449a9fc9400ae
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2024-07-25 01:03:36 +0900

    Fix: import listRouter 중복 삭제

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import { specs } from '@config/swagger.config.js';
 import { status } from '@config/response.status.js';
 import { response } from '@config/response.js';
 import { pool } from '@config/db.config.js';
-import {listRouter} from '@routes/list.route.js';
 import {alertRouter} from '@routes/alert.route.js';
 import { userRouter } from '@routes/user.route.js';
 import { listRouter } from '@routes/list.route.js';
