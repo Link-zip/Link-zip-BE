5a8c99e63cdad6a25f3b85e5d6fef034ca715fa8
haeun <haeun9634@naver.com>
2024-07-30 22:02:15 +0900

    [#23] Feat: 최근저장/좋아요 리스트 구현

--- src/models/list.dao.js
+++ src/models/list.dao.js
@@ -3,7 +3,7 @@
 import { pool } from "@config/db.config.js";
 import { BaseError } from "@config/error.js";
 import { status } from "@config/response.status.js";
-import { getUnviewListByUserId, getZipById } from "./list.sql.js";
+import { getUnviewListByUserId, getZipById, getLikeListByUserId, getRecentListByUserId } from "./list.sql.js";
 
 //Zip의 정보를 가져오는 함수
 export const getZip = async (zipId) => {
@@ -46,6 +46,98 @@ export const getPreviewUnviewList = async (userId, sort, filter) => {
             }
         }
         else query += " ORDER BY created_at DESC"; //쿼리가 없으면 기본값, 최신순으로 
+        const [links] = await pool.query(query, parseInt(userId));
+        
+        // links에 대해 zip 정보 추가
+        const linksWithZip = await Promise.all(links.map(async (link) => {
+            const zip = await getZip(link.zip_id);
+            return { ...link, zip };
+        }));
+        
+        conn.release();
+        return linksWithZip;
+    
+    } catch (err) {
+        throw new BaseError(status.BAD_REQUEST);
+    }
+}
+
+//좋아요 링크를 조회하는 함수
+export const getPreviewLikeList = async(userId, sort, filter)=>{
+    try {
+        const conn = await pool.getConnection();
+        let query = getLikeListByUserId;
+        
+        // 필터 조건 추가
+        if (filter) {
+            if (filter === "onlylink") {//link만
+                query += " AND tag = 'link' ";
+            } else if (filter === "onlytext") {//text만
+                query += " AND tag = 'text' ";
+            }
+        }
+        
+        // 정렬 조건 추가
+        if (sort) {
+            if (sort === "recent") {//최신순
+                query += " ORDER BY created_at DESC";
+            } else if (sort === "past") {//과거순
+                query += " ORDER BY created_at ASC";
+            } else if (sort === "dictionary") {//사전순
+                query += " ORDER BY title ASC, created_at DESC";
+            } else if (sort === "visit") {//방문순
+                query += " ORDER BY visit DESC, created_at DESC";
+            }
+        }
+        else query += " ORDER BY created_at DESC"; //쿼리가 없으면 기본값, 최신순으로 
+
+        const [links] = await pool.query(query, parseInt(userId));
+
+
+        // links에 대해 zip 정보 추가
+        const linksWithZip = await Promise.all(links.map(async (link) => {
+            const zip = await getZip(link.zip_id);
+            return { ...link, zip };
+        }));
+        
+        conn.release();
+        return linksWithZip;
+    
+    } catch (err) {
+        throw new BaseError(status.BAD_REQUEST);
+    }
+}
+
+//최근 저장 링크를 조회하는 함수 
+export const getPreviewRecentList = async(userId, sort, filter)=>{
+    try {
+        const conn = await pool.getConnection();
+
+        let query = getRecentListByUserId;
+        
+        // 필터 조건 추가
+        if (filter) {
+            if (filter === "onlylink") {//link만
+                query += " AND tag = 'link' ";
+            } else if (filter === "onlytext") {//text만
+                query += " AND tag = 'text' ";
+            }
+        }
+        
+        // 정렬 조건 추가
+        if (sort) {
+            if (sort === "recent") {//최신순
+                query += " ORDER BY created_at DESC";
+            } else if (sort === "past") {//과거순
+                query += " ORDER BY created_at ASC";
+            } else if (sort === "dictionary") {//사전순
+                query += " ORDER BY title ASC, created_at DESC";
+            } else if (sort === "visit") {//방문순
+                query += " ORDER BY visit DESC, created_at DESC";
+            }
+        }
+        else query += " ORDER BY created_at DESC"; //쿼리가 없으면 기본값, 최신순으로 
+
         const [links] = await pool.query(query, parseInt(userId));
         
         // links에 대해 zip 정보 추가
--- src/models/list.sql.js
+++ src/models/list.sql.js
@@ -2,3 +2,9 @@
 export const getUnviewListByUserId = "SELECT * FROM link WHERE user_id = ? AND visit = 0";
 
 export const getZipById = "SELECT * FROM zip z WHERE z.id = ?";
+
+//좋아요 리스트
+export const getLikeListByUserId = "SELECT * FROM link WHERE user_id = ? AND link.like = 1";
+//최근저장
+export const getRecentListByUserId = "SELECT * FROM link WHERE user_id = ? AND created_at >= DATE_SUB(NOW(), INTERVAL 3 DAY)";
+
