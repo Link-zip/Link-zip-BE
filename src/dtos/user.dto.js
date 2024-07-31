6438dd2ebd2c79256ab6da5f59005bee54e2c52f
Seungbo Shim <fodo9898@gmail.com>
2024-08-01 00:32:43 +0900

    [#28] Refactor: 닉네임 중복 체크 수정 및 index.js fix

--- src/dtos/user.dto.js
+++ src/dtos/user.dto.js
@@ -6,11 +6,3 @@ export const userResponseDTO = (data) => {
         createdAt: data.createdAt,
     }
 }
-
-/** 닉네임 중복 여부 반환 DTO */
-export const nicknameResponseDTO = (data) => {
-    return {
-        isValid: data.count === 0,
-        message: data.count === 0 ? "환상적인 닉네임이에요!" : "이미 사용 중인 유저가 있어요!",
-    }
-}
