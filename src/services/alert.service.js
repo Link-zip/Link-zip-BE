eaaff3f7dfd40955ece7f119136bd956d2d55acf
haeun <haeun9634@naver.com>
2024-08-08 00:32:30 +0900

    Refactor 알림 코드 수정

--- src/services/alert.service.js
+++ src/services/alert.service.js
@@ -2,8 +2,8 @@
 
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
-import {alertAddResponseDTO, alertPreviewResponseDTO, alertConfirmResponseDTO, alertDeleteResponseDTO } from "../dtos/alert.dto.js";
-import {addAlert, getAlert, getLink,  getUserAlert, AlertConfirm, AlertDelete} from "../models/alert.dao.js";
+import {alertAddResponseDTO, alertPreviewResponseDTO, alertConfirmResponseDTO } from "../dtos/alert.dto.js";
+import {addAlert, getAlert, getLink,  getUserAlert, AlertConfirm} from "../models/alert.dao.js";
 
 //알림 생성
 export const prepareAlertData = async (body) => {
@@ -42,11 +42,4 @@ export const checkAlert = async (data) => {
     const AlertData = await AlertConfirm(data.alertId);
     if(AlertData==-1) throw new BaseError(status.ALERT_NOT_FOUND); 
     return alertConfirmResponseDTO();
-}
-
-//알림 삭제
-export const eraseAlert = async(data) => {
-    const AlertData = await AlertDelete(data.alertId);
-    if(AlertData==-1) throw new BaseError(status.ALERT_NOT_FOUND); 
-    return alertDeleteResponseDTO();
 }
