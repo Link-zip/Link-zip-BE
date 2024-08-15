b966278ffe4af04f1c135e30e7111d4cf19ec14f
GodUser1005 <jijone1005@naver.com>
2024-08-15 22:31:31 +0900

    [#52] Refactor: zip조회 응답 형식 변경, swagger 변경

--- src/providers/zip.provider.js
+++ src/providers/zip.provider.js
@@ -2,5 +2,7 @@ import { getZipsDao } from "@models/zip.dao.js"
 import { getZipResDto } from "@dtos/zip.dto.js";
 
 export const getZipsProvider = async (req) => {
-    return (await getZipsDao(req.query.sort, req.userId)).map((zip) => getZipResDto(zip));
+    return {
+      zips : ((await getZipsDao(req.query.sort, req.userId)).map((zip) => getZipResDto(zip)))
+    };
 }
