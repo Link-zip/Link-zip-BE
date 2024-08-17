39562b951f03d9e443e0dba6631f04bdd1fa6c7e
Seungbo Shim <fodo9898@gmail.com>
2024-08-17 19:13:06 +0900

    [#54] Refactor: finally구문을 이용한 커넥션 release 처리

--- src/models/user.dao.js
+++ src/models/user.dao.js
@@ -6,9 +6,9 @@ import { checkNicknameSql, insertUserSql, selectUserSql, selectUserByKakaoIdSql 
 
 /** 회원가입 DAO, id 리턴 */
 export const addUserDao = async (data) => {
+    let conn;
     try {
-        const conn = await pool.getConnection();
-        
+        conn = await pool.getConnection();
         const [[isValid]] = await conn.query(checkNicknameSql, data.nickname);
         if (isValid.count > 0) {
             conn.release();
@@ -24,43 +24,54 @@ export const addUserDao = async (data) => {
     } catch (error) {
         console.error(error);
         throw new BaseError(status.BAD_REQUEST);
+    } finally {
+        if (conn) conn.release();
     }
 }
 
 /** 사용자 정보 조회 DAO */
 export const getUserDao = async (userId) => {
+    let conn;
     try {
-        const conn = await pool.getConnection();
+        conn = await pool.getConnection();
         const [[result]] = await conn.query(selectUserSql, userId);
         conn.release();
         return result;
     } catch (error) {
         console.error(error);
         throw new BaseError(status.BAD_REQUEST);
-    }   
+    } finally {
+        if (conn) conn.release();
+    }
 }
 
 export const getUserByKakaoIdDao = async (kakaoId) => {
+    let conn;
     try {
-        const conn = await pool.getConnection();
+        conn = await pool.getConnection();
         const [[result]] = await conn.query(selectUserByKakaoIdSql, kakaoId);
         conn.release();
         return result;
     } catch (error) {
         console.error(error);
         throw new BaseError(status.BAD_REQUEST);
+    } finally {
+        if (conn) conn.release();
     }
 }
 
 /** 닉네임 중복 체크 DAO */
 export const checkNicknameDao = async (nickname) => {
+    let conn;
     try {
-        const conn = await pool.getConnection();
+        conn = await pool.getConnection();
         const [[result]] = await conn.query(checkNicknameSql, nickname);
         conn.release();
         return result; // count 반환
     } catch (error) {
         console.error(error);
         throw new BaseError(status.BAD_REQUEST);
+    } finally {
+        if (conn) conn.release();
     }
 }
